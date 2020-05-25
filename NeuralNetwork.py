061735690975696a90ff3bcd9c08161e0050fd42
morgan-vanv <morganvanv@gmail.com>
2020-05-25 16:32:07 -0400

    Added sigmoid functions

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -47,4 +47,12 @@ class Network(object):
         print('# of OL Neurons: ' + str(self.neuronsOL))
         print('# of HLs: ' + str(self.numberHL))
 
-    pass
+# Math Functions
+
+    # Sigmoid Function
+    def sigmoid(z):
+        return 1.0/(1.0+np.exp(-z))
+
+    # Derivative of Sigmoid
+    def sigmoid_prime(z):
+        return sigmoid(z)*(1-sigmoid(z))
